d7bfe63e723c3008109022bb5058b3cc959db0cd
danand <danand1101@gmail.com>
2022-10-07 10:28:19 +0200

    plotly

---
+++ Viz/PLOTLY/DASH/minimal_app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from dash import Dash, callback, html, dcc, Input, Output
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+# Some example data and its corresponding Plotly figure
+df = pd.DataFrame(np.random.rand(10,2), columns=['A', 'B'])
+fig = px.scatter(df, x='A', y='B')
+
+# Structure of my page
+app.layout = html.Div([
+    html.H1('A simple dashboard'),
+    html.Button('Generate Random Data', id='randomize', n_clicks=0),
+    html.Div(id='display-value'),
+    dcc.Graph(id="myGraph", figure=fig),
+])
+
+@callback(
+    Output(component_id='display-value', component_property='children'),
+    Output(component_id='myGraph', component_property='figure'),
+    Input(component_id='randomize', component_property='n_clicks'),
+)
+def do_something(n_clicks):
+    # Regenerate the figure
+    df = pd.DataFrame(np.random.rand(10,2), columns=['A', 'B'])
+    fig = px.scatter(df, x='A', y='B')
+
+    return [
+        f'You have clicked {n_clicks} times',
+        fig,
+    ]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
